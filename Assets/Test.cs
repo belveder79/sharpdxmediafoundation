75c589241eeee5bff1e23d2405bbe9a9650b8b51
arth <clemens@arth.co.at>
2018-10-05 20:48:38 +0200

    initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if !UNITY_EDITOR
+using SharpDX.MediaFoundation;
+#endif
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
